d617a09d9421ac936836d957aeb6449f83a5d615
Norbert Vígh <norbertusvigh@gmail.com>
2024-12-02 09:08:57 +0100

    day 2 done

---
+++ src/day02.ts
@@ -0,0 +1,38 @@
+const parse = (s: string) => s.trim().split('\n');
+
+export const safeReports = (s: string) => {
+	return parse(s)
+		.map((line) => {
+			return isReportSafe(line.split(' ').map(Number));
+		})
+		.reduce((acc, x) => acc + (x ? 1 : 0), 0);
+};
+exports.first = safeReports;
+
+const isReportSafe = (arr: number[]) => {
+	return arr.reduce((acc, x, i, arr) => {
+		return (
+			acc &&
+			(arr[1] > arr[0]
+				? i == 0 || (x > arr[i - 1] && Math.abs(x - arr[i - 1]) <= 3)
+				: i == 0 || (x < arr[i - 1] && Math.abs(x - arr[i - 1]) <= 3))
+		);
+	}, true);
+};
+
+export const safeReportsWithDampener = (s: string) => {
+	return parse(s)
+		.map((line) => {
+			const arr = line.split(' ').map(Number);
+			return (
+				isReportSafe(arr) ||
+				arr
+					.map((x, i, arr) =>
+						isReportSafe([...arr.slice(0, i), ...arr.slice(i + 1)])
+					)
+					.some((x) => x)
+			);
+		})
+		.reduce((acc, x) => acc + (x ? 1 : 0), 0);
+};
+exports.second = safeReportsWithDampener;
---
+++ tests/day02.test.ts
@@ -0,0 +1,16 @@
+import { safeReports, safeReportsWithDampener } from '../src/day02';
+
+const input = `7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9`;
+
+test('day 2-1', () => {
+	expect(safeReports(input)).toBe(2);
+});
+
+test('day 2-2', () => {
+	expect(safeReportsWithDampener(input)).toBe(4);
+});
